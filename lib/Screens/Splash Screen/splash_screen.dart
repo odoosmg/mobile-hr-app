2abeb12c263714584abda49d8fe1aae765f7aabf
seaklim <kimseaklim83@gmail.com>
2024-10-14 11:54:11 +0700

    update splash screen logo

--- lib/Screens/Splash Screen/splash_screen.dart
+++ lib/Screens/Splash Screen/splash_screen.dart
@@ -1,9 +1,9 @@
 // ignore_for_file: use_build_context_synchronously, depend_on_referenced_packages, library_private_types_in_public_api
 
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:hrm_employee/GlobalComponents/others/loading_inidicator.dart';
-import 'package:hrm_employee/Models/auth/app_local.dart';
 import 'package:hrm_employee/Screens/Authentication/sign_in.dart';
 import 'package:hrm_employee/Screens/Home/home_screen.dart';
 import 'package:hrm_employee/Services/app_services.dart';
@@ -11,19 +11,19 @@ import 'package:hrm_employee/Services/database_service.dart';
 import 'package:hrm_employee/extensions/textstyle_extension.dart';
 import 'package:nb_utils/nb_utils.dart';
 
-import '../../GlobalComponents/button_global.dart';
-import '../../GlobalComponents/purchase_model.dart';
 import '../../constant.dart';
 import 'on_board.dart';
 
 class SplashScreen extends StatefulWidget {
-  const SplashScreen({Key? key}) : super(key: key);
+  const SplashScreen({super.key});
 
   @override
   _SplashScreenState createState() => _SplashScreenState();
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+  double logoSize = 0.1;
+
   @override
   void initState() {
     super.initState();
@@ -31,7 +31,14 @@ class _SplashScreenState extends State<SplashScreen> {
   }
 
   Future<void> init() async {
-    await Future.delayed(const Duration(seconds: 2));
+    /// Animation
+    Future.delayed(const Duration(milliseconds: 50)).then((_) {
+      setState(() {
+        logoSize = 1;
+      });
+    });
+
+    await Future.delayed(const Duration(milliseconds: 2400));
 
     defaultBlurRadius = 10.0;
     defaultSpreadRadius = 0.5;
@@ -48,14 +55,6 @@ class _SplashScreenState extends State<SplashScreen> {
         const OnBoard().launch(context, isNewTask: true);
       }
     }
-
-    // bool isValid = await PurchaseModel().isActiveBuyer();
-    // if(isValid){
-    //   finish(context);
-    //   const OnBoard().launch(context, isNewTask: true);
-    // } else{
-    //   showLicense(context: context);
-    // }
   }
 
   @override
@@ -76,12 +75,22 @@ class _SplashScreenState extends State<SplashScreen> {
               height: MediaQuery.of(context).size.height / 2.5,
             ),
 
-            Text(
-              "Soma Group",
-              style: Theme.of(context).textTheme.whiteS20W700NoChange,
+            // Text(
+            //   "Soma Group",
+            //   style: Theme.of(context).textTheme.whiteS20W700NoChange,
+            // ),
+
+            AnimatedScale(
+              scale: logoSize,
+              duration: const Duration(seconds: 2),
+              curve: Curves.fastOutSlowIn,
+              child: const Image(
+                image: AssetImage('images/app_logo_primary.png'),
+                // width: logoSize,
+              ),
             ),
-            10.height,
-            const CustomCircularProgressindicator(),
+            // 10.kHeight,
+            // const CustomCircularProgressindicator(),
 
             // const Image(
             //   image: AssetImage('images/round_logo.png'),
